1225f307b3c1992210171cb9542772640511c4a8
Venelin Valkov <venelin@bapplz.com>
2015-05-15 13:10:42 +0300

    Create link for different platforms

--- src/index.js
+++ src/index.js
@@ -9,12 +9,19 @@ var HuntMe = function(apiKey) {
     return this
 }
 
-HuntMe.prototype.createLink = function* createLink(link, platform) {
-    payload = {links: [{
-            url: link,
-            platform: platform
-        }]
-    }
+/**
+ * Creates new short link pointing to different links depending on the user device
+ * Example:
+ *  yield huntMe.createLink([
+ *      {url: "facebook.com", platform: "default"}, 
+ *      {url: "market://details?id=com.facebook.katana", platform: "android"}
+ *  ])
+ *
+ * @param links - Must contain platform: "default" link e.g. see Example
+ */
+HuntMe.prototype.createLink = function* (links) {
+
+    payload = {links: links}
     var options = {
         uri: this.config.createLink,
         method: "POST",
